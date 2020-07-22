c5048b70d1b3388ea9a53f1c096e0d03a6a2e3a2
osotorrio <osotorrio@hotmail.com>
2020-07-22 17:00:27 +0100

    Added application code example

---
+++ GangOfFour.Patterns/Structural/Observer/ApplicationCode.cs
@@ -0,0 +1,25 @@
+﻿using GangOfFour.Patterns.Structural.Observer.Subjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GangOfFour.Patterns.Structural.Observer
+{
+    public class ApplicationCode
+    {
+        [Fact]
+        public void ExampleOfUse()
+        {
+            IAmCustomerObserver customerBobby = new BuyAutomaticallyCustomerObserver(Guid.NewGuid(), "Robert Fischer");
+            IAmCustomerObserver customerTigran = new GoodToKnowCustomerObserver(Guid.NewGuid(), "Tigran Petrosian");
+
+            BookProductSubject kasparovBook = new BookProductSubject("Garry Kasparov on My Great Predecessors", "978-1781945155", 19.95m);
+
+            kasparovBook.Attach(customerBobby);
+            kasparovBook.Attach(customerTigran);
+
+            kasparovBook.Price = 14.99m;
+        }
+    }
+}
