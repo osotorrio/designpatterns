c7edf8c8a4c22a30476680dd314ed59f4425b8ae
osotorrio <osotorrio@hotmail.com>
2021-01-16 23:30:07 +0100

    Refactored test

--- GangOfFour.Patterns/Creational/Builder/Client/Application.cs
+++ GangOfFour.Patterns/Creational/Builder/Client/Application.cs
@@ -24,23 +24,21 @@ namespace GangOfFour.Patterns.Creational.Builder.Client
             var priceStandardHoliday = priceBuilder.Build();
             priceStandardHoliday.CalculateTotalPrice().ShouldBe(1482.50M);
 
+            packages.BuildStandardHolidayPackage(scheduleBuilder);
+            var scheduleStarndardHoliday = scheduleBuilder.Build();
+            scheduleStarndardHoliday.PrintoutSchedule().ShouldBe(
+                $"Holidays from 30-Dec-21 9:00:00 AM to 06-Jan-22 9:00:00 AM{_nl}Staying in hotel from 30-Dec-21 12:00:00 PM to 06-Jan-22 12:00:00 PM{_nl}");
+
             priceBuilder.Reset();
+            scheduleBuilder.Reset();
 
             packages.BuildVipHolidayPackage(priceBuilder);
             var priceVipHoliday = priceBuilder.Build();
             priceVipHoliday.CalculateTotalPrice().ShouldBe(2217.50m);
 
-
-            packages.BuildStandardHolidayPackage(scheduleBuilder);
-            var scheduleStarndardHoldiays = scheduleBuilder.Build();
-            scheduleStarndardHoldiays.PrintoutSchedule().ShouldBe(
-                $"Holidays from 30-Dec-21 9:00:00 AM to 06-Jan-22 9:00:00 AM{_nl}Staying in hotel from 30-Dec-21 12:00:00 PM to 06-Jan-22 12:00:00 PM{_nl}");
-
-            scheduleBuilder.Reset();
-
             packages.BuildVipHolidayPackage(scheduleBuilder);
-            var scheduleVipHoldiays = scheduleBuilder.Build();
-            scheduleVipHoldiays.PrintoutSchedule().ShouldBe(
+            var scheduleVipHoliday = scheduleBuilder.Build();
+            scheduleVipHoliday.PrintoutSchedule().ShouldBe(
                 $@"Holidays from 30-Dec-21 9:00:00 AM to 06-Jan-22 9:00:00 AM{_nl}Staying in hotel from 30-Dec-21 12:00:00 PM to 06-Jan-22 12:00:00 PM{_nl}Fun in the theme park on the date 30-Dec-21 2:00:00 PM{_nl}Having dinner on the date 30-Dec-21 7:00:00 PM{_nl}Party out all night on the date 30-Dec-21 10:00:00 PM{_nl}");
         }
     }
