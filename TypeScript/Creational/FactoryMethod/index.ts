c43d702108c8e40a8e7849005fa5e0fd2ea99563
Oscar Sotorrio <oscar.sotorrio@ascentsoftware.eu>
2022-01-26 22:51:24 +0100

    Set up type script project

---
+++ TypeScript/Creational/FactoryMethod/index.ts
@@ -0,0 +1,64 @@
+ abstract class Creator {
+
+    public abstract factoryMethod(): Product;
+
+    public someOperation(): string {
+        const product = this.factoryMethod();
+        // Now, use the product.
+        return `Creator: The same creator's code has just worked with ${product.operation()}`;
+    }
+}
+
+
+class ConcreteCreator1 extends Creator {
+    public factoryMethod(): Product {
+        return new ConcreteProduct1();
+    }
+}
+
+class ConcreteCreator2 extends Creator {
+    public factoryMethod(): Product {
+        return new ConcreteProduct2();
+    }
+}
+
+
+interface Product {
+    operation(): string;
+}
+
+/**
+ * Concrete Products provide various implementations of the Product interface.
+ */
+class ConcreteProduct1 implements Product {
+    public operation(): string {
+        return '{Result of the ConcreteProduct1}';
+    }
+}
+
+class ConcreteProduct2 implements Product {
+    public operation(): string {
+        return '{Result of the ConcreteProduct2}';
+    }
+}
+
+/**
+ * The client code.
+ */
+function clientCode(creator: Creator) {
+    // ...
+    console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
+    console.log(creator.someOperation());
+    // ...
+}
+
+/**
+ * The Application picks a creator's type depending on the configuration or
+ * environment.
+ */
+console.log('App: Launched with the ConcreteCreator1.');
+clientCode(new ConcreteCreator1());
+console.log('');
+
+console.log('App: Launched with the ConcreteCreator2.');
+clientCode(new ConcreteCreator2());
